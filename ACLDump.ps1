a5599e4a0decd6cb4280badd469512bf169ca671
rensl0w <127228607+rensl0w@users.noreply.github.com>
2023-03-12 15:41:19 -0500

    Add files via upload

---
+++ ACLDump.ps1
@@ -0,0 +1,25 @@
+﻿# List all folders in the specified directory and its subdirectories
+$FolderPath = Get-ChildItem -Path "\\servername\location" -Directory -Recurse -Force
+
+# Create an empty array to store the ACL information
+$Report = @()
+
+# Loop through each folder and retrieve the ACL information
+foreach ($Folder in $FolderPath) {
+    $Acl = Get-Acl -Path $Folder.FullName
+    foreach ($Access in $Acl.Access) {
+        $Properties = [ordered]@{
+            'FolderName' = $Folder.FullName
+            'AD Group or User' = $Access.IdentityReference
+            'Permissions' = $Access.FileSystemRights
+            'Inherited' = $Access.IsInherited
+        }
+        $Report += New-Object -TypeName PSObject -Property $Properties
+    }
+}
+
+# Export the ACL report to the Excel worksheet
+$Report | Export-Excel -WorksheetName 'ACL Report' -AutoSize -BoldTopRow
+
+# Close and save the Excel workbook
+Close-PowerShellExcel -Save
